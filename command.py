2dc716448638b726f3c5a7950e85fc2a35a8754e
Nick Oliver <github@pixnbits.org>
2014-08-10 22:14:44 -0700

    execute npm in the directory of the currently focused file so now this actually works ;)

--- command.py
+++ command.py
@@ -1,4 +1,4 @@
-import subprocess, json
+import subprocess, json, os
 import sublime, sublime_plugin
 
 class NpmCommand(object):
@@ -6,30 +6,25 @@ class NpmCommand(object):
 		json.loads(str(json_string))
 
 	#TODO def find_npm(self)
-		#find npm's bin path so we don't need to use `shell=True` with subprocess
+		#find npm's bin path so we don't need to use `shell=True` with subprocess (lookin' at you, Windows)
 		#self.npm_path = ...
 
 	def run_npm(self, commands):
-		file_name = self.view.file_name()
-		sublime.status_message("file_name? "+str(file_name))
-		#proc = subprocess.Popen(['echo','blue'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		#proc = subprocess.Popen(['npm']+commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=file_name)
-		#proc = subprocess.Popen([ 'cd', file_name,'\n;\n', 'npm']+commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=file_name)
-		proc = subprocess.Popen([ 'dir' ], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=file_name)
+		dir_name = os.path.dirname(self.view.file_name())
+		proc = subprocess.Popen(['npm']+commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=dir_name)
 		out, err = proc.communicate()
 		return_code = proc.poll()
-		# works: subprocess.call(['npm', 'version'], shell=True)
 		#sublime.status_message("proc exited with code "+str(return_code))
 		#show results
 		window = sublime.active_window()
 		output_view = window.get_output_panel("textarea")
 		window.run_command("show_panel", {"panel": "output.textarea"})
 		output_view.set_read_only(False)
+		# sections commented out are the sublime v2 way, AFAIK they're the only bits preventing v2 compatability
 		# edit = output_view.begin_edit()
 		# output_view.insert(edit, output_view.size(), "Hello, World!")
-		#output_view.run_command("append", {"characters": "Installing package '"+pkg+"'..."})
+		# TODO: pretty-fy the feedback to the user...err OR out show? npm warnings enough to show err? even use output panel?
 		output_view.run_command("append", {"characters": "Out: "+out.decode("utf-8")})
-		#output_view.run_command("append", {"characters": "Out: "+self.parse_json(out)})
 		output_view.run_command("append", {"characters": "Err: "+err.decode("utf-8")})
 		# output_view.end_edit(edit)
 		output_view.set_read_only(True)
