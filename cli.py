f119f6a4ec3accdb1811c50af56e5ac9557d2548
Nick Oliver <github@pixnbits.org>
2014-08-12 19:11:55 -0700

    better method of npm discovery and invocation Popen is platform specific :'(

---
+++ cli.py
@@ -0,0 +1,55 @@
+# copied from https://github.com/benschwarz/sublime-bower/blob/master/bower/utils/cli.py and modified
+# original license:
+# Copyright © 2013 Ben Schwarz
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# this is a modified version (ex: overcome python bug 6689)
+
+import sublime
+import os
+import subprocess
+
+if os.name == 'nt':
+    LOCAL_PATH = ';' + os.getenv('APPDATA') + '\\npm'
+    BINARY_NAME = 'npm.cmd'
+else:
+    LOCAL_PATH = ':/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin'
+    BINARY_NAME = 'npm'
+
+os.environ['PATH'] += LOCAL_PATH
+
+class CLI():
+    def find_binary(self):
+        print("find_binary")
+        for dir in os.environ['PATH'].split(os.pathsep):
+            path = os.path.join(dir, BINARY_NAME)
+            if os.path.exists(path):
+                return path
+        sublime.error_message(BINARY_NAME + ' could not be found in your $PATH. Check the installation guidelines - https://github.com/PixnBits/sublime-text-npm')
+
+    def execute(self, command, cwd):
+        binary = self.find_binary()
+        command.insert(0, binary)
+
+        cflags = 0
+
+        if os.name == 'nt':
+            cflags = 0x08000000  # Avoid opening of a cmd on Windows (CREATE_NO_WINDOW)
+
+        # per http://bugs.python.org/issue6689 Popen handles commands differently on Win vs *nix
+        # see http://stackoverflow.com/a/1254322/2770309
+        if os.name != 'nt':
+            command = " ".join(command)
+        proc = subprocess.Popen(command, shell=True, cwd=cwd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, creationflags=cflags)
+
+        stdout_data, stderr_data = proc.communicate()
+        returncode = proc.wait()
+
+        if stdout_data:
+            stdout_data = stdout_data.decode('utf-8')
+        if stderr_data:
+            stderr_data = stderr_data.decode('utf-8')
+
+        return [returncode, stdout_data, stderr_data]
--- command.py
+++ command.py
@@ -1,26 +1,18 @@
 import subprocess, json, os
 import sublime, sublime_plugin
+from .cli import CLI
 
-class NpmCommand(object):
+class NpmCommand(CLI):
 	def parse_json(self, json_string):
 		#json.loads(str(json_string))
 		return json.loads(json_string)
-
-	#TODO def find_npm(self)
-		#find npm's bin path so we don't need to use `shell=True` with subprocess (lookin' at you, Windows)
-		#self.npm_path = ...
 
 	def run_npm(self, commands):
 		active_file_name = self.view.file_name()
 		if not active_file_name:
 			return [-1, "", "no focused file to look at parent npm package"]
 		dir_name = os.path.dirname(active_file_name)
-		proc = subprocess.Popen(['npm']+commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=dir_name)
-		out, err = proc.communicate()
-		return_code = proc.poll()
-		#sublime.status_message("proc exited with code "+str(return_code))
-		out = out.decode("utf-8")
-		err = err.decode("utf-8")
+		return_code, out, err = self.execute(commands, dir_name)
 		# return the process result
 		return [return_code, out, err]
 
@@ -40,7 +32,12 @@ class NpmCommand(object):
 	def show_npm_output(self, output):
 		return_code, out, err = output
 		# TODO: pretty-fy the feedback to the user...err OR out show? npm warnings enough to show err? even use output panel?
-		message = "Out: "+out+"\nErr: "+err
+		message = ""
+		if out:
+			message = message + "Out: " + out
+		if err:
+			message = message + "\nErr: " + err
+		#message = "Out: "+out+"\nErr: "+err
 		#show results
 		self.output_textarea(message)
 
