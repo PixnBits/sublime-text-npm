1c4f24cfc4506407dead7ce427da27dbf86b45b9
Nick Oliver <github@pixnbits.org>
2014-08-17 21:38:20 -0700

    better get of npm config value doh!

--- link.py
+++ link.py
@@ -15,28 +15,27 @@ class NpmLinkRegisterCommand(NpmLink, sublime_plugin.TextCommand):
 class NpmLinkUseCommand(NpmLink, sublime_plugin.TextCommand):
 	def run(self, edit):
 		# find where npm links packages
-		code, config_text, err = self.run_npm(['config', 'list', '-l'])
-		if config_text:
-			prefix = re.search("\r|\nprefix\s*=\s*\"(.+)\"", config_text).group(1)
-			if prefix:
-				#self.output_textarea("prefix: "+str(prefix))
-				# now list out the packages at npm/node_modules
-				module_dir = os.path.join(os.path.normpath(prefix), 'node_modules')
-				#self.output_textarea("module_dir: "+module_dir)
-				# check for directories?
-				self.package_names = []
-				for entry in os.listdir(module_dir):
-					if os.path.isdir( os.path.join(module_dir, entry) ):
-						self.package_names.append(entry)
-						# could parse package.json to get version, etc
-				# show our formatted results to the user
-				window = sublime.active_window()
-				window.show_quick_panel(self.package_names, self.link_package_index, sublime.MONOSPACE_FONT)
-
-			else:
-				self.output_textarea("prefix not found :'(\n"+config_text)
+		code, prefix, err = self.run_npm(['config', 'get', 'prefix'])
+		if prefix:
+			# remove whitespace, such as trailing EOL chars
+			prefix = prefix.strip()
+			#self.output_textarea("prefix: "+str(prefix))
+			# now list out the packages at npm/node_modules
+			module_dir = os.path.join(os.path.normpath(prefix), 'node_modules')
+			#self.output_textarea("module_dir: "+module_dir)
+			# check for directories?
+			self.package_names = []
+			for entry in os.listdir(module_dir):
+				if os.path.isdir( os.path.join(module_dir, entry) ):
+					self.package_names.append(entry)
+					# could parse package.json to get version, etc
+			# show our formatted results to the user
+			window = sublime.active_window()
+			window.show_quick_panel(self.package_names, self.link_package_index, sublime.MONOSPACE_FONT)
+		elif err:
+			self.output_textarea("error getting prefix config: \n"+config_text)
 		else:
-			self.output_textarea("no config_text :'(")
+			self.output_textarea("prefix not found :'(\n"+config_text)
 
 
 	def link_package(self, package_name):
